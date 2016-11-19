63785831ab124b38634757d84540ae3dcf982803
Alex Dao <azd2@duke.edu>
2016-11-19 18:21:31 -0500

    Add BiasFinder singleton

--- src/BiasFinder.java
+++ src/BiasFinder.java
@@ -1,7 +1,38 @@
+import java.util.HashSet;
 
-public class BiasFinder {
-	private BiasFinder(){
-		
-	}
+final class BiasFinder {
+
+    private static final BiasFinder INSTANCE = new BiasFinder();
+    private HashSet<String> conservativeSources = new HashSet<>();
+    private HashSet<String> liberalSources = new HashSet<>();
+
+    enum Bias {CONSERVATIVE, LIBERAL, UNKNOWN}
+
+    private BiasFinder() {
+        conservativeSources.add("Fox");
+        conservativeSources.add("Blaze");
+        conservativeSources.add("Breitbart");
+
+        liberalSources.add("NY");
+        liberalSources.add("WSJ");
+        liberalSources.add("CNN");
+        liberalSources.add("MSNBC");
+    }
+
+    static BiasFinder getInstance() {
+        return INSTANCE;
+    }
+
+    Bias findBias(String source) {
+        if(conservativeSources.contains(source)) {
+            return Bias.CONSERVATIVE;
+        }
+        else if(liberalSources.contains(source)) {
+            return Bias.LIBERAL;
+        }
+        else {
+            return Bias.UNKNOWN;
+        }
+    }
 
 }
